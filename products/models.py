ce7a925dcebfcba950cbce45c1eae96088083bf8
pauldardis <paul.dardis@bt.com>
2020-04-18 17:53:32 +0000

    styling on product details

--- products/models.py
+++ products/models.py
@@ -6,14 +6,14 @@ class Product(models.Model):
     subcatagory = models.CharField(max_length=254, default='')
     name = models.CharField(max_length=254, default='')
     description = models.TextField(default='')
-    brief_description = models.TextField(default='')
-    full_description = models.TextField(default='')
+    brief_description = models.TextField(null = True, blank=True)
+    full_description = models.TextField(null = True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
-    image1 = models.ImageField(upload_to='images',default='')
-    image2 = models.ImageField(upload_to='images',default='')
-    image3 = models.ImageField(upload_to='images',default='')
-    image4 = models.ImageField(upload_to='images',default='')
+    image = models.ImageField(upload_to='images',null = True, blank=True)
+    image1 = models.ImageField(upload_to='images',null = True, blank=True)
+    image2 = models.ImageField(upload_to='images',null = True, blank=True)
+    image3 = models.ImageField(upload_to='images',null = True, blank=True)
+    image4 = models.ImageField(upload_to='images',null = True, blank=True)
 
     def __str__(self):
         return self.name
