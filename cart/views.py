6ca3819f796e3a8f6d61d509120ebaea8d319ac0
pauldardis <paul.dardis@bt.com>
2020-04-25 19:03:09 +0000

    changing styling on cards

--- cart/views.py
+++ cart/views.py
@@ -35,3 +35,5 @@ def adjust_cart(request, id):
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+    
--- products/models.py
+++ products/models.py
@@ -6,15 +6,14 @@ class Product(models.Model):
     catagory = models.CharField(max_length=254, choices=PRODUCT_CATAGORY)
     subcatagory = models.CharField(max_length=254, choices=PRODUCT_SUBCATAGORY)
     name = models.CharField(max_length=254, default='')
-    description = models.TextField(default='')
-    brief_description = models.TextField(null = True, blank=True)
-    full_description = models.TextField(null = True, blank=True)
+    product_summary = models.TextField(default='')
+    product_description = models.TextField(null = True, blank=True)
+    product_features = models.TextField(null = True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images',null = True, blank=True)
     image1 = models.ImageField(upload_to='images',null = True, blank=True)
     image2 = models.ImageField(upload_to='images',null = True, blank=True)
     image3 = models.ImageField(upload_to='images',null = True, blank=True)
-    image4 = models.ImageField(upload_to='images',null = True, blank=True)
+   
 
     def __str__(self):
         return self.name
