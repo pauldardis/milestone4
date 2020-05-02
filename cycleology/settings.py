8bb42e86fd1297647b09c0597b5e1d30c14e11f6
pauldardis <paul.dardis@bt.com>
2020-05-02 20:00:28 +0000

    changing home page and styling on footer

--- cycleology/settings.py
+++ cycleology/settings.py
@@ -168,3 +168,5 @@ STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
