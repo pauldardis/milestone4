8fb1bc0bc5a95b97c03e85dcbe4fd7fa78458679
pauldardis <paul.dardis@bt.com>
2020-05-06 14:34:09 +0000

    changing urls for home and all products

--- cycleology/urls.py
+++ cycleology/urls.py
@@ -30,7 +30,6 @@ from .settings import MEDIA_ROOT
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home, name='index'),
-    # url(r'^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
